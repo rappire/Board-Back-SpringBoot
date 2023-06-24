f2f897f1cf407e2bad04a0c4c5d8686932a3bb11
rappire <rappire@gmail.com>
2023-06-25 04:49:33 +0900

    fix : annotation

--- src/main/java/com/example/week4/config/WebConfig.java
+++ src/main/java/com/example/week4/config/WebConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * CROS 설정 모든 origin 과 모든 method 를 받고 있음
+ */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
